539e493fa9cf1fb33dd8e5cbbfb1e50e983d338c
Wiliam Traoré <hi@wil-liam.com>
2024-04-02 11:22:04 +0200

    feat(organization-preview): create component

---
+++ src/components/OrganizationPreview.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { BaseIcon } from "magma-design-system-test";
+
+const props = defineProps({
+  logo: {
+    type: String,
+    default: null
+  },
+  name: {
+    type: String,
+    default: "Acme Inc."
+  }
+});
+</script>
+
+<template>
+  <div class="bg-background-subtle w-full h-72 rounded-2xl pt-10 pl-36">
+    <div class="rounded-tl-lg overflow-hidden h-full flex">
+      <div class="w-1/2 h-full bg-background-emphasis pt-8 px-6">
+        <div class="flex items-center gap-2 mb-12">
+          <div
+            class="w-8 h-8 bg-background-subtle rounded-full flex items-center justify-center"
+          >
+            <BaseIcon v-if="!props.logo" icon="image" />
+            <img
+              v-else
+              :src="props.logo"
+              alt="Organization logo"
+              class="w-8 h-8 rounded-full"
+            />
+          </div>
+          <span class="text-white mgm-txt-sm">{{ props.name }}</span>
+        </div>
+        <div>
+          <div class="flex gap-2 mb-6">
+            <BaseIcon icon="image" color="#CBD5E1" />
+            <div class="w-20 h-4 bg-[#808286] rounded"></div>
+          </div>
+          <div class="flex gap-2 mb-6">
+            <BaseIcon icon="image" color="#CBD5E1" />
+            <div class="w-20 h-4 bg-[#808286] rounded"></div>
+          </div>
+          <div class="flex gap-8">
+            <div class="w-[1px] h-52 bg-foreground-default"></div>
+            <div>
+              <div class="w-14 h-4 bg-[#808286] rounded mb-6"></div>
+              <div class="w-12 h-4 bg-[#808286] rounded"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-white w-1/2 border-t border-border-subtle">
+        <div class="p-8 w-[500px] border-b border-border-subtle">
+          <div class="w-full h-5 bg-background-subtle rounded mb-4"></div>
+          <div class="w-full h-8 bg-background-subtle rounded"></div>
+        </div>
+        <div class="p-8 w-[500px]">
+          <div
+            class="w-full h-40 bg-background-subtle rounded-lg border border-border-subtle"
+          ></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/signup/SignupOrganizationView.vue
+++ src/views/signup/SignupOrganizationView.vue
@@ -2,13 +2,39 @@
 // NPM
 
 // Components
-// import {
-//   BaseIcon,
-//   FieldAvatarUploader,
-//   FieldInput
-// } from "magma-design-system-test";
+import { FieldAvatarUploader, FieldInput } from "magma-design-system-test";
+import OrganizationPreview from "../../components/OrganizationPreview.vue";
 </script>
 
 <template>
-  <div class="w-[200px] h-[500px] bg-red-500">test</div>
+  <div class="mx-32 mt-40 w-[600px] max-w-full box-content">
+    <h1 class="text-mgm-txt-xl mb-6">
+      {{ $t("signupOrganizationView.heading") }}
+    </h1>
+
+    <form class="mb-10">
+      <FieldInput
+        :label="$t('signupOrganizationView.form.organization')"
+        type="text"
+        required
+        placeholder="Acme Inc."
+        class="mb-4"
+      />
+      <FieldInput
+        :label="$t('generic.website')"
+        type="text"
+        required
+        placeholder="https://acme.com"
+        class="mb-4"
+      />
+      <FieldAvatarUploader
+        id="logo"
+        :cta="$t('signupOrganizationView.form.logo.cta')"
+        :description="$t('signupOrganizationView.form.logo.description')"
+        label="Logo"
+        placeholder-icon="image"
+      />
+    </form>
+    <OrganizationPreview />
+  </div>
 </template>
